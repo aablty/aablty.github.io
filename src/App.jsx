b010ae84a0bab7c66ed91fdcea725bce44815a9d
aablty <acrouple@yandex.ru>
2025-05-24 18:56:43 +0500

    TEST

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import Header from '@/components/Header/Header.jsx'
+import Header from "@/components/Header/Header.jsx";
 
 export default function App() {
-
   return (
     <>
       <Header />
-      
+      <h1>TEST</h1>
     </>
-  )
+  );
 }
-
-

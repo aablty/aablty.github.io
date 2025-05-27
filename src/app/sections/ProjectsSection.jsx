8828b21ae195ef3931df88af4957a085de67d45a
aablty <acrouple@yandex.ru>
2025-05-28 00:44:47 +0500

    fix link

--- src/app/sections/ProjectsSection.jsx
+++ src/app/sections/ProjectsSection.jsx
@@ -1,3 +1,5 @@
+import { NavLink } from "react-router-dom";
+
 import Subtitle from "@/app/components/Subtitle";
 import "@/assets/styles/ProjectsSection.css";
 
@@ -6,9 +8,9 @@ export default function ProjectsSection() {
     <section className="projects">
       <div className="projects_header">
         <Subtitle>projects</Subtitle>
-        <a href="/projects" className="projects_link">
+        <NavLink to="/projects" className="projects_link">
           View all {'~~>'}
-        </a>
+        </NavLink>
       </div>
       <div className="project_list"></div>
     </section>

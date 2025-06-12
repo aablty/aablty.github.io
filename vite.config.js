d2357f797b97a9b384f55e5df6a8f373f4557fa8
aablty <acrouple@yandex.ru>
2025-06-12 11:38:44 +0500

    small fixes

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://aablty.vercel.app",
-        changeOrigin: true,
-        secure: true,
-      },
-    },
-  },
+  plugins: [react()]
 });
